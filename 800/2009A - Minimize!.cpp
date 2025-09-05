bcbe37f837ccd56f03b84ac75b5b411ad6604806
kunalavghade <kunalavghade77@gmail.com>
2025-09-05 23:11:28 +0530

    2009A - Minimize

---
+++ 800/2009A - Minimize!.cpp
@@ -0,0 +1,21 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+    int n, a, b;
+    cin >> n;
+
+    while(n--) {
+        cin >> a >> b;
+        cout<<b-a<<"\n";
+    }
+
+
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    solve();
+    return 0;
+}
