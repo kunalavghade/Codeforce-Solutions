79f69a4b4540ec40dcca18712470c5723a11aaf5
kunalavghade <kunalavghade77@gmail.com>
2025-09-07 13:10:36 +0530

    80A - Panoramix's Prediction

---
+++ 800/80A - Panoramix's Prediction.cpp
@@ -0,0 +1,44 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+bool primes[51] = {true};
+
+void calcPrimes() {
+    for(int i = 0; i <= 50; i++) {
+        primes[i] = true;
+    }
+    primes[0] = primes[1] = false;
+    for(int i = 2; i <= 50; i++){
+        if(primes[i]) {
+            for(int j = i*i; j <= 50; j+=i) {
+                primes[j] = false;
+            }
+        }
+    }
+}
+
+void solve() {
+    calcPrimes();
+    int a, b;
+    cin >> a >> b;
+
+    if(primes[a] && primes[b]) {
+        for(int i = a+1; i < b; i++) {
+            if(primes[i]) {
+                cout << "NO\n";
+                return;
+            }
+        }
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    solve();
+    return 0;
+}
