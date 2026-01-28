3400fddf12ea638db8ef72f9be20608f1982b2ea
Kunal Avghade <67228832+kunalavghade@users.noreply.github.com>
2026-01-28 23:39:17 +0530

    2091B - Team Training (#4)

---
+++ 800/2091B - Team Training.cpp
@@ -0,0 +1,35 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+void solve() {
+   int n, k;
+   cin>>n>>k;
+
+   vector<int> a(n);
+   for(int i=0; i<n; i++) 
+      cin>>a[i];
+   
+    sort(begin(a), end(a));
+
+    int ans = 0, len = 0;
+    for(int i = n-1; i >= 0; i--) {
+        len++;
+        if(a[i] * len >= k) {
+            ans++;
+            len = 0;
+        }
+    }
+    cout<<ans<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
